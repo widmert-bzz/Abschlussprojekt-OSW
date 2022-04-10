166e1e08bbdf229ccebeb37e538c31088d2f7ce7
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-04-10 21:42:19 +0200

    Bug

--- First Projekt/Assets/Scripts/Healthbar.cs
+++ First Projekt/Assets/Scripts/Healthbar.cs
@@ -3,18 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Healthbar : MonoBehaviour
 {
-    public Slider slider;
+   
+
+    public void Start()
+    {
+       
+    }
+
 
     public void SetMaxHealth(int health)
     {
-        slider.maxValue = health;
-        slider.value = health;  
+
+
     }
 
     public void SetHealth(int health)
     {
-        slider.value = health;
+        
     }
 }
