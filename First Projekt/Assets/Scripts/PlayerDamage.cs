8da4c86cf6654cb4d6c9d5375b69208e3f6fd4af
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-04-13 18:24:47 +0200

    Updates + scale 16 + fixed Colliders

--- First Projekt/Assets/Scripts/PlayerDamage.cs
+++ First Projekt/Assets/Scripts/PlayerDamage.cs
@@ -14,15 +14,7 @@ public class PlayerDamage : MonoBehaviour
         currentHealth = maxHealth;
         //healthBar.SetMaxHealth(maxHealth);
     }
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Enemy")
-        {
-            TakeDamage(10);
-
-
-        }
-    }
+   
 
     void TakeDamage(int damage)
     {
