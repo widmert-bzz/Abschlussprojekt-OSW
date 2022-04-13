09dfd5d080a876255b454d4f7a991d174f191cdc
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-04-13 17:37:12 +0200

    Car

--- First Projekt/Assets/Scripts/PlayerMovement.cs
+++ First Projekt/Assets/Scripts/PlayerMovement.cs
@@ -2,20 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Playermovement : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+
     public float moveSpeed;
     public Rigidbody2D rb;
+    public Camera cam;
+    Vector2 mousePos;
+    private Vector2 moveDirection;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
+        ProcessInputs();
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    void FixedUpdate()
+    {
+
+
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+
+        Move();
     }
 }
