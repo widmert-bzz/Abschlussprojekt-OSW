666544ccbd981d73c6adc4b2720ab1bcc2d34ec9
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-05-05 12:35:17 +0200

    .

--- First Projekt/Assets/Scripts/PlayerMovement.cs
+++ First Projekt/Assets/Scripts/PlayerMovement.cs
@@ -5,25 +5,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    public float walkingDuration;
     public float speed;
+    public float volume;
     public Rigidbody2D rb;
     public Camera cam;
+    public AudioClip walkingSound;
     Vector2 mousePos;
     private Vector2 moveDirection;
-
+    private float moveX = 0f;
+    private float moveY = 0f;
+    private float timer;
 
     void Update()
     {
         ProcessInputs();
 
         mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        if (timer < 0 && (moveX != 0 || moveY != 0))
+        {
+            AudioSource.PlayClipAtPoint(walkingSound, gameObject.transform.position, volume);
+            timer = walkingDuration;
+        }
+
+        timer -= Time.deltaTime;
     }
 
     void ProcessInputs()
     {
-        float moveX = Input.GetAxisRaw("Horizontal");
-        float moveY = Input.GetAxisRaw("Vertical");
+        moveX = Input.GetAxisRaw("Horizontal");
+        moveY = Input.GetAxisRaw("Vertical");
 
         moveDirection = new Vector2(moveX, moveY).normalized;
     }
