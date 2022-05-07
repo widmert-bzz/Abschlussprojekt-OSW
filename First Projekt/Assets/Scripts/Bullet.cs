b9f6ef925beb718d5a8e93886977299194de796e
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-05-07 03:23:41 +0200

    7/5 Corner + Blood

--- First Projekt/Assets/Scripts/Bullet.cs
+++ First Projekt/Assets/Scripts/Bullet.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public GameObject hitEffect;
     public float bulletDamage = 10;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.tag != "bullet")
         {
-            GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-            Destroy(effect, 1f);
+            
             Destroy(gameObject);
 
         }
--- First Projekt/Assets/Scripts/Damage.cs
+++ First Projekt/Assets/Scripts/Damage.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public float health = 20;
+    public ParticleSystem blood;
+
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "bullet")
         {
+            CreateParticles();
             health -= 10;
             if (health <= 0)
             {
                 Destroy(gameObject);
             }
         }
+    }
+
+    void CreateParticles()
+    {
+        blood.Play();
     }
 }
