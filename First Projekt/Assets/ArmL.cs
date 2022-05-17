cc624b3a013cdc69d51156fd298225a6278870df
Tinoxxxmit3x <widmer.tino@gmail.com>
2022-05-17 14:59:03 +0200

    Bugs

--- First Projekt/Assets/ArmL.cs
+++ First Projekt/Assets/ArmL.cs
@@ -7,19 +7,39 @@ public class ArmL : MonoBehaviour
 {
     public float armHitPosition = 100;
     public float armRestPosition = 35;
-    public float hitDelay = 5;
+    public float hitDelay = 5f;
+    public float armSpeed = 5f;
     public Rigidbody2D rb;
     private float timer;
+    private ArmState armState = ArmState.Resting;
 
     private void Update()
     {
         timer -= Time.deltaTime;
     }
+
+    private void FixUpdate()
+    {
+        if (armState == ArmState.Punching)
+        {
+            if( rb.rotation < 100)
+            {
+                rb.rotation = armSpeed * Time.deltaTime;
+            }
+           
+        }
+    }
     public void Punch()
     {
         if (timer < 0)
         {
-            rb.rotation = armHitPosition;
+            armState = ArmState.Punching;
         }
+    }
+    enum ArmState
+    {
+        Punching,
+        Returning,
+        Resting
     }
 }
--- First Projekt/Assets/ArmR.cs
+++ First Projekt/Assets/ArmR.cs
@@ -5,21 +5,38 @@ using UnityEngine;
 
 public class ArmR : MonoBehaviour
 {
-    public float armHitPosition = 100;
+    public float armHitPosition = -100;
     public float armRestPosition = 35;
     public float hitDelay = 5;
     public Rigidbody2D rb;
     private float timer;
+    private ArmState armState = ArmState.Resting;
 
     private void Update()
     {
-        timer -= Time.deltaTime; 
+        timer -= Time.deltaTime;
+
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if (armState == ArmState.Punching)
+        {
+            rb.rotation = Mathf.Lerp(rb.rotation, armRestPosition, Time.deltaTime * timer);
+        }
     }
     public void Punch()
     {
         if(timer < 0)
         {
-            rb.rotation = armHitPosition;
+            armState = ArmState.Punching;
         } 
+    }
+    enum ArmState
+    {
+        Punching,
+        Returning,
+        Resting
     }
 }
